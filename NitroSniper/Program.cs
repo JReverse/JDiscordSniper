f36a97249a6612ff0edcef70e7b03d6333521621
J <40630518+JReverse@users.noreply.github.com>
2022-09-19 19:58:42 -0500

    Init commit

---
+++ NitroSniper/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Discord;
+using Discord.Gateway;
+
+namespace NitroSniper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sniper_token = "";
+
+            DiscordSocketClient client = new DiscordSocketClient();
+            client.OnLoggedIn += Client_OnLoggedIn;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.Login(sniper_token);
+
+            Console.ReadLine();
+        }
+
+        private static void Client_OnMessageReceived(DiscordSocketClient client, MessageEventArgs args)
+        {
+            const string giftPrefix = "discord.gift/";
+
+            var match = Regex.Match(args.Message.Content, giftPrefix + ".{16,24}");
+
+            if (match.Success)
+            {
+                string code = match.Value.Substring(match.Value.IndexOf(giftPrefix) + giftPrefix.Length);
+
+                try
+                {
+                    string your_token = "";
+
+                    DiscordClient your_client = new DiscordClient();
+                    your_client.Token = your_token;
+                    your_client.RedeemGift(code);
+
+                    Console.WriteLine("Successfully redeemed code " + code);
+                }
+                catch (DiscordHttpException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
+        {
+            Console.WriteLine("Logged in");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # JDiscordSniper
-.Net 6
+- NET 6
+- Lightweight
+- Stable
+- Linux (Don't use on windows Linux is better for 24/7 running)
+
+# Setup
+- Ubuntu NET 6 (https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu)
+- Centos NET 6 (https://docs.microsoft.com/en-us/dotnet/core/install/linux-centos)
+- Debian NET 6 (https://docs.microsoft.com/en-us/dotnet/core/install/linux-debian)
+- Edit sniper_token to your alt account that is in many discord token
+- Edit your_token your token to get the nitros
+- Building Linux (`dotnet publish -r linux-x64`)
+
+# Usage
+- (Linux) `screen dotnet NitroSniper.dll`
